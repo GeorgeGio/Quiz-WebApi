d1c8b6cb3c59e9a902fbf53496608dc99f9b7300
George <georgeoustapas@gmail.com>
2023-01-17 22:02:38 +0000

    added questions html

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,8 +1,43 @@
-let questions = [{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
+let questions = [{
+    question: "Q1",
+    answers: ["ex1", "ex2", "ex3", "ex4"],
+    correctAnswer: 0
+},
 
-{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2}
+{
+    question: "Q2",
+    answers: ["ex1", "ex2", "ex3", "ex4"],
+    correctAnswer: 2
+},
+{
+    question: "Q3",
+    answers: ["ex1", "ex2", "ex3", "ex4"],
+    correctAnswer: 2
+},
+{
+    question: "Q4",
+    answers: ["ex1", "ex2", "ex3", "ex4"],
+    correctAnswer: 3
+}
 
 
 ]
 
 let currentQuestion = 0;
+let countdownTimer = 0;
+let questionArea = document.querySelector("#question-box")
+
+function renderQuestions() {
+
+    let quest1 = document.createElement("p");
+    quest1.textContent = questions[questionIndex].question;
+    questionArea.appendChild(quest1);
+
+    for (let i = 0; i < questions[questionIndex].answers.length; i++) {
+        let answerButton = document.createElement["button"];
+        answerButton.textContent = questions[questionIndex].answers[i];
+        
+        
+    }
+    
+}
